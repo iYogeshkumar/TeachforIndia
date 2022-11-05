cacd0df7e9accee85dc9ab41fbfe78d747144658
yogesh Kumar <iamyogeshkumar21@gmail.com>
2022-11-05 21:27:37 +0530

    registration page ui created

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from './Components/Navbar';
+import Register from './Pages/Register';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Register/>
+    </div>
+  );
+}
+
+export default App;
