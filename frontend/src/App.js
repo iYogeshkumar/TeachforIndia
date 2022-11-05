4e93ee1bba7cfa069f6f9a0bf340381113c96cfd
yogesh Kumar <iamyogeshkumar21@gmail.com>
2022-11-05 23:33:26 +0530

    adminlogin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
 import Navbar from './Components/Navbar';
+import AdminLogin from './Pages/AdminLogin';
 import Register from './Pages/Register';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Register/>
+      <Register/> 
+      {/* <AdminLogin/> */}
     </div>
   );
 }
---
+++ frontend/src/Pages/ListOfCandidates.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListOfCandidates = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ListOfCandidates
